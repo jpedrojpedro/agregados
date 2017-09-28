5a53c5e8913873bae1ecac87c8f65822396b5b1b
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2017-09-27 22:15:07 -0300

    Adição de link para bookmark
    Dessa forma, o usuário, estando no Facebook, clica no bookmark gerado
    referente ao evento para cadastrar o convidado.

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -4,14 +4,16 @@
     name: 'app',
     firebase () {
       return {
-        guests: database.ref('development' + this.event.name + '/convidados'),
-        tags: database.ref('development' + this.event.name + '/tags')
+        guests: database.ref('development/' + this.event.name + '/convidados'),
+        tags: database.ref('development/' + this.event.name + '/tags')
       }
     },
     data () {
+      let e = this.$route.path.substring(1)
       return {
         event: {
-          name: this.$route.path
+          name: e,
+          link: "javascript:(function()%7Bvar%20fb%20%3D%20%22https%3A%2F%2Ffacebook.com%22%20%2B%20window.location.pathname%3Bvar%20name%20%3D%20document.querySelector('%23fb-timeline-cover-name').innerText%3Bvar%20url%20%3D%20%22https%3A%2F%2Fus-central1-agregados-311b9.cloudfunctions.net%2FaddGuest%22%3Bvar%20query%20%3D%20%22%3FeventName%3D" + e + "%26facebook%3D%22%20%2B%20fb%20%2B%20%22%26nome%3D%22%20%2B%20name%3Bvar%20fullurl%20%3D%20url%20%2B%20query%3Bvar%20img%20%3D%20document.createElement('img')%3Bimg.src%20%3D%20fullurl%3Bdocument.body.appendChild(img)%7D)()"
         }
       }
     }
@@ -26,6 +28,9 @@
       </div>
       <div class="col">
         <h2>Lista: {{ event.name }}</h2>
+      </div>
+      <div class="col">
+        <h2><a :href="event.link">Arraste para Bookmark</a></h2>
       </div>
     </div>
     <div class="row">
